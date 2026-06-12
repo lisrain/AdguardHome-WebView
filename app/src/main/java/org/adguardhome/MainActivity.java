db893d90c3b366a669c977908fe3f4ecaa594138
lisrain <liangcheng1130@163.com>
2026-06-12 23:16:55 +0800

    Fix: normal status bar + immersive nav bar + back gesture + HyperOS compat

--- app/src/main/java/org/adguardhome/MainActivity.java
+++ app/src/main/java/org/adguardhome/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends Activity {
         webView = new WebView(this);
         setContentView(webView);
 
-        applyInsets();
+        setupWindow();
 
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
@@ -51,35 +51,39 @@ public class MainActivity extends Activity {
         webView.loadUrl(TARGET_URL);
     }
 
-    private void applyInsets() {
+    private void setupWindow() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            WindowInsetsController controller = getWindow().getInsetsController();
+            Window window = getWindow();
+            WindowInsetsController controller = window.getInsetsController();
             if (controller != null) {
+                controller.setSystemBarsAppearance(
+                    WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                    WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+                );
+                controller.show(WindowInsets.Type.statusBars());
                 controller.hide(WindowInsets.Type.navigationBars());
                 controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
             }
             webView.setOnApplyWindowInsetsListener((v, insets) -> {
                 Rect statusBar = insets.getInsets(WindowInsets.Type.statusBars()).toRect();
-                v.setPadding(0, statusBar.height(), 0, 0);
+                Rect navBar = insets.getInsets(WindowInsets.Type.navigationBars()).toRect();
+                v.setPadding(0, statusBar.height(), 0, navBar.bottom);
                 return insets;
             });
         } else {
             hideSystemUI();
-            webView.post(() -> {
-                Rect rect = new Rect();
-                getWindow().getDecorView().getWindowVisibleDisplayFrame(rect);
-                webView.setPadding(0, rect.top, 0, 0);
-            });
         }
     }
 
     private void hideSystemUI() {
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(
-            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         );
         webView.post(() -> {
             Rect rect = new Rect();
@@ -92,7 +96,7 @@ public class MainActivity extends Activity {
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) {
-            applyInsets();
+            setupWindow();
         }
     }
 
@@ -101,7 +105,7 @@ public class MainActivity extends Activity {
         if (webView.canGoBack()) {
             webView.goBack();
         } else {
-            super.onBackPressed();
+            finish();
         }
     }
 
