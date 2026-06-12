fa2794ee581642e80504870da9527a49cb070b1a
lisrain <liangcheng1130@163.com>
2026-06-12 23:40:29 +0800

    Fix: keep nav bar visible, set status bar color, request insets

--- app/src/main/java/org/adguardhome/MainActivity.java
+++ app/src/main/java/org/adguardhome/MainActivity.java
@@ -59,6 +59,10 @@ public class MainActivity extends Activity {
 
     private void setupWindow() {
         Log.d(TAG, "setupWindow, SDK=" + Build.VERSION.SDK_INT);
+        
+        getWindow().setStatusBarColor(0xFFF5F5F5);
+        getWindow().setNavigationBarColor(0x00000000);
+        
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             Window window = getWindow();
             WindowInsetsController controller = window.getInsetsController();
@@ -69,7 +73,6 @@ public class MainActivity extends Activity {
                     WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
                 );
                 controller.show(WindowInsets.Type.statusBars());
-                controller.hide(WindowInsets.Type.navigationBars());
                 controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
             } else {
                 Log.w(TAG, "controller is null");
@@ -82,7 +85,95 @@ public class MainActivity extends Activity {
                 Log.d(TAG, "padding set: top=" + statusInsets.top + " bottom=" + navInsets.bottom);
                 return insets;
             });
+            webView.requestApplyInsets();
             Log.d(TAG, "onApplyWindowInsetsListener set");
+        } else {
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
+        Rect rect = new Rect();
+        getWindow().getDecorView().getWindowVisibleDisplayFrame(rect);
+        Log.d(TAG, "hideSystemUI: visibleDisplayFrame top=" + rect.top);
+        webView.setPadding(0, rect.top, 0, 0);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        Log.d(TAG, "onWindowFocusChanged focus=" + hasFocus);
+        if (hasFocus) {
+            setupWindow();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        Log.d(TAG, "onBackPressed canGoBack=" + webView.canGoBack());
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            finish();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy");
+        if (webView != null) {
+            webView.destroy();
+        }
+        super.onDestroy();
+    }
+}
+        });
+
+        webView.setWebChromeClient(new WebChromeClient());
+
+        webView.loadUrl(TARGET_URL);
+        Log.d(TAG, "loadUrl done");
+    }
+
+    private void setupWindow() {
+        Log.d(TAG, "setupWindow, SDK=" + Build.VERSION.SDK_INT);
+        
+        getWindow().setStatusBarColor(0xFFF5F5F5);
+        getWindow().setNavigationBarColor(0x00000000);
+        
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            Window window = getWindow();
+            WindowInsetsController controller = window.getInsetsController();
+            if (controller != null) {
+                Log.d(TAG, "controller found");
+                controller.setSystemBarsAppearance(
+                    WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                    WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+                );
+                controller.show(WindowInsets.Type.statusBars());
+                controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+            } else {
+                Log.w(TAG, "controller is null");
+            }
+            ViewCompat.setOnApplyWindowInsetsListener(webView, (v, insets) -> {
+                Insets statusInsets = insets.getInsets(WindowInsets.Type.statusBars());
+                Insets navInsets = insets.getInsets(WindowInsets.Type.navigationBars());
+                Log.d(TAG, "insets: statusBars.top=" + statusInsets.top + " navigationBars.bottom=" + navInsets.bottom);
+                v.setPadding(0, statusInsets.top, 0, navInsets.bottom);
+                Log.d(TAG, "padding set: top=" + statusInsets.top + " bottom=" + navInsets.bottom);
+                return WindowInsetsCompat.CONSUMED;
+            });
+            ViewCompat.requestApplyInsets(webView);
+            Log.d(TAG, "ViewCompat.setOnApplyWindowInsetsListener set");
         } else {
             hideSystemUI();
         }
